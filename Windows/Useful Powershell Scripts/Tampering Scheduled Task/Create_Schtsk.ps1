9ad127e6517d747fdf952ab63e4ff607abdd9fb0
June Twain <thejunefire@gmail.com>
2024-01-26 17:14:18 +0700

    Create Create_Schtsk.ps1

---
+++ Windows/Useful Powershell Scripts/Tampering Scheduled Task/Create_Schtsk.ps1
@@ -0,0 +1,8 @@
+﻿$trg1 = New-ScheduledTaskTrigger -AtLogon
+$trg2 = New-ScheduledTaskTrigger -Once -At 01:00 -RepetitionInterval (New-TimeSpan -Minutes 15)
+$trg1.Repetition = $trg2.Repetition
+$actn = New-ScheduledTaskAction -Execute "PowerShell" -Argument "E:\.temp\.PBSG\4. QC Done\05. SAP RDP\Install.ps1"
+$princ = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount
+
+Register-ScheduledTask -TaskName "PBSG KakaoTalk Suspend Update" -Trigger $trg1 -Action $actn -Principal $princ
+
